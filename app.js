3045787a69ca202f4a6a942dc0dd0b3f95ccc56f
Conan <conanmalanaphy@gmail.com>
2021-02-15 17:29:22 +0000

    Error message

--- app.js
+++ app.js
@@ -6,5 +6,11 @@ app.get('/', (req,res) =>{
 	res.send('Hello World, This is conan. I love my Lentil, she is wonderful!');
 });
 
+app.get('/demo', (req,res) =>{
+	res.set("x-full-stack", "4life");
+	res.status(418);
+	res.send('Hello World, This is conan. I love my Lentil, she is wonderful!');
+});
+
 app.listen(port,() => console.log(`example app listening on port ${port}!`));
 
